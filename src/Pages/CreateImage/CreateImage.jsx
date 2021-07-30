8e725003780463dd19db0e2eebbf6401bd68c22d
solomon-igbigbi <solomonigbigbi07@gmail.com>
2021-07-30 05:25:45 +0100

    changes

--- src/Pages/CreateImage/CreateImage.jsx
+++ src/Pages/CreateImage/CreateImage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import Bottle from "../../Assets/Bottles_plus_lOGO.svg"
 import Logo from "../../Assets/Logo.png"
@@ -7,10 +7,15 @@ import * as htmlToImage from 'html-to-image';
 
 
 import "./Create.scss"
+import { FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton, WhatsappIcon, WhatsappShareButton } from 'react-share'
 
 function CreateImage() {
   const history = useHistory()
   const user = JSON.parse(localStorage.getItem("user"))
+  const [shareButton, setShareButton] = useState(true)
+  const [shareImage, setShareImage] = useState(null)
+  const [ setIsDownload] = useState(false)
+  
 
   useEffect(() => {
     if(user === undefined || user === null ) {
@@ -18,19 +23,47 @@ function CreateImage() {
     }
   })
 
-  const download = () => {
+  const uploadImageToCloudinary = async (file) => {
+    const data = new FormData()
+    data.append("file", file)
+    data.append("upload_preset", "umfs48jg")
+    const res = await fetch("https://api.cloudinary.com/v1_1/dpregnexq/image/upload", {
+      method: "POST",
+      body: data
+    })
+    return res.json()
+  }
+
+  
+
+  const preDownload = async () => {
+    setIsDownload(true)
+    download()
+  }
+
+  const download = useCallback(() => {
     let node = document.querySelector('.imageHolder');
     let poster = document.querySelector('.poster');
 
     htmlToImage.toPng(node)
-      .then(function (dataUrl) {
-        poster.href = dataUrl;
-        poster.click()
+      .then(async function (dataUrl) {
+        if(shareImage === null) {
+          console.log(shareImage)
+          const res = await  uploadImageToCloudinary(dataUrl)
+          return setShareImage(res.secure_url)
+        } else {
+          poster.href = dataUrl;
+          poster.click()
+        }
       })
       .catch(function (error) {
         console.error('oops, something went wrong!', error);
       });
-  }
+  }, [shareImage])
+
+  useEffect(() => {
+    download()
+  },[download])
 
   return (
     <div className="body2">
@@ -55,11 +88,25 @@ function CreateImage() {
       <div className="overlay"></div>
       <div className="download">
         <a className="poster"  href="/" download="poster">null</a>
-        <p>Your banner is ready</p><br/>
+        <p>Your banner is ready</p>
         <span className="span">Kindly click the button below to download</span>
-        <button onClick={download}>
+        <button onClick={() => preDownload()}>
           download
         </button>
+        {shareButton ? <button className="btn2" onClick={() => setShareButton(false)}>
+          share
+        </button> :
+        <div className="socials">
+          <FacebookShareButton url={shareImage}>
+            <FacebookIcon round={true}/>
+          </FacebookShareButton>
+          <WhatsappShareButton  url={shareImage}>
+            <WhatsappIcon round={true}/>
+          </WhatsappShareButton>
+          <TwitterShareButton url={shareImage}>
+            <TwitterIcon round={true}/>
+          </TwitterShareButton>
+        </div>}
       </div>
     </div>
   )
